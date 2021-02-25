a047175c0da695941904582faeed7034416f46ee
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-02-25 12:40:50 -0500

    Parameterized routes

---
+++ server.js
@@ -0,0 +1,44 @@
+const express = require('express');
+
+const app = express();
+
+const PORT = 3000;
+
+const friends = [
+  {
+    id: 0,
+    name: 'Albert Einstein'
+  },
+  {
+    id: 1,
+    name: 'Sir Isaac Newton'
+  }
+];
+
+app.get('/friends', (req, res) => {
+  res.json(friends);
+});
+
+app.get('/friends/:friendId', (req, res) => {
+  const friendId = Number(req.params.friendId);
+  const friend = friends[friendId];
+  if (friend) {
+    res.status(200).json(friend);
+  } else {
+    res.status(404).json({
+      error: "Friend does not exist"
+    });
+  }
+});
+
+app.get('/messages', (req, res) => {
+  res.send('<ul><li>Helloo Albert!</li></ul>');
+});
+
+app.post('/messages', (req, res) => {
+  console.log('Updating messages...');
+});
+
+app.listen(PORT, () => {
+  console.log(`Listening on ${PORT}...`);
+});
