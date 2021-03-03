a3d4d8b1318290cc93eda0e808c311027beb020e
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-03-03 07:47:03 -0500

    express.static middleware

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,9 +1,7 @@
 const path = require('path');
-// /folder/files.jpg \folder\files.jpg
 
 function getMessages(req, res) {
-  res.sendFile(path.join(__dirname, '..', 'public', 'skimountain.jpg'));
-  // res.send('<ul><li>Helloo Albert!</li></ul>');
+  res.sendFile(path.join(__dirname, '..', 'public', 'images', 'skimountain.jpg'));
 }
 
 function postMessage(req, res) {
--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 const express = require('express');
+const path = require('path');
 
 const friendsRouter = require('./routes/friends.router');
 const messagesRouter = require('./routes/messages.router');
@@ -14,6 +15,7 @@ app.use((req, res, next) => {
   console.log(`${req.method} ${req.baseUrl}${req.url} ${delta}ms`);
 });
 
+app.use('/site', express.static(path.join(__dirname, 'public')));
 app.use(express.json());
 
 app.use('/friends', friendsRouter);
