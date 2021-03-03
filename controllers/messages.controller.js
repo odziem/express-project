aafd3a953bee971df9bd249c95c95458bd2ad90a
Adam Odziemkowski <odziem@users.noreply.github.com>
2021-03-03 05:28:26 -0500

    Sending files

--- controllers/messages.controller.js
+++ controllers/messages.controller.js
@@ -1,5 +1,9 @@
+const path = require('path');
+// /folder/files.jpg \folder\files.jpg
+
 function getMessages(req, res) {
-  res.send('<ul><li>Helloo Albert!</li></ul>');
+  res.sendFile(path.join(__dirname, '..', 'public', 'skimountain.jpg'));
+  // res.send('<ul><li>Helloo Albert!</li></ul>');
 }
 
 function postMessage(req, res) {
